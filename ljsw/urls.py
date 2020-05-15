4c2e387e4915aab635c97c03625bf919b5741949
Yichr <59714936+Yichr@users.noreply.github.com>
2020-05-16 00:19:18 +0800

    Update urls.py

--- ljsw/urls.py
+++ ljsw/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path('ljsw/user/', include('user.urls')),
     path('ljsw/index/', include('index.urls')),
     path('ljsw/live/', include('Live.urls')),
-    path('ljsw/live/recycling', include('Recycling.urls')),
 ]
