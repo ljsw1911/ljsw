6814e4c441edc60abc6e099f320c5074a90dd034
Shiyao1994 <554943787@qq.com>
2020-05-18 13:38:28 +0800

    更新cart中登录保护

--- ljsw/Cart/views.py
+++ ljsw/Cart/views.py
@@ -5,10 +5,19 @@ from django.http import JsonResponse
 from Cart.models import Cart
 
 
-# 获取购物车的信息
 def cart(request):
+    # 加入登录保护，判断用户是否登录
     token = request.GET.get('token')
-    user_id = cache.get(token)
+    try:
+        user_id = cache.get(token)
+    except Exception as e:
+        return JsonResponse({
+            'status': 201,
+            'msg': 'token失效，请重新登录',
+            'data': {}
+        })
+
+    # 获取购物车的信息
     if request.method == 'POST':
         goods_id = request.POST.get('goods_id')
         cart_new = Cart(user_id=user_id, goods_id=goods_id)
