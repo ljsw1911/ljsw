741a824f3dd1b633ef42d60b90e55513047c1d88
Shiyao1994 <554943787@qq.com>
2020-05-22 19:34:49 +0800

    添加cart中的商品数量

--- ljsw/Cart/views.py
+++ ljsw/Cart/views.py
@@ -5,25 +5,17 @@ from django.http import JsonResponse
 from Cart.models import Cart
 
 
+# 获取购物车的信息
 def cart(request):
-    # 加入登录保护，判断用户是否登录
     token = request.GET.get('token')
-    try:
-        user_id = cache.get(token)
-    except Exception as e:
-        return JsonResponse({
-            'status': 201,
-            'msg': 'token失效，请重新登录',
-            'data': {}
-        })
-
-    # 获取购物车的信息
+    user_id = cache.get(token)
     if request.method == 'POST':
         goods_id = request.POST.get('goods_id')
-        cart_new = Cart(user_id=user_id, goods_id=goods_id)
+        cart_num = request.POST.get('cart_num')
+        cart_new = Cart(user_id=user_id, goods_id=goods_id, cart_num=cart_num)
         cart_new.save()
         cart_id = cart_new.cart_id
-        cart_dict = {'cart_id': cart_id, 'user_id': user_id, 'goods_id': goods_id}
+        cart_dict = {'cart_id': cart_id, 'user_id': user_id, 'goods_id': goods_id, 'cart_num':cart_num}
         print(cart_dict)
         return JsonResponse(
             {
